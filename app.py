c0461e35223ef218f1e120516ec914ffca797423
DavidME1604 <davidestebanmoralese@gmail.com>
2024-11-15 00:27:17 -0500

    fix get params

--- app.py
+++ app.py
@@ -1,20 +1,33 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-from interes import obtener_interes
-
 
 app = Flask(__name__)
 CORS(app)
-@app.route('/')
-def home():
-    return "Hola, Render!"
+
+
+def obtener_interes(initial_capital, final_capital, num_periods, periodic_contribution):
+    # Ejemplo de cálculo de interés (ajusta según tu fórmula)
+    if num_periods == 0:
+        return "Número de períodos no puede ser cero"
+
+    interest_rate = ((final_capital - initial_capital - (periodic_contribution * num_periods)) / (
+                initial_capital * num_periods)) * 100
+    return interest_rate
+
 
 @app.route('/api/calculate', methods=['POST'])
 def calculate():
     data = request.json
-    input_values = data.get('input', 0)
-    result = obtener_interes(input_values['initialCapital'],input_values['finalCapital'],input_values['numPeriods'],input_values['periodicContribution'],)
+
+    initial_capital = data.get('initialCapital', 0)
+    final_capital = data.get('finalCapital', 0)
+    num_periods = data.get('numPeriods', 0)
+    periodic_contribution = data.get('periodicContribution', 0)
+
+    result = obtener_interes(initial_capital, final_capital, num_periods, periodic_contribution)
+
     return jsonify({'result': result})
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
