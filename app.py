1d51f86d72322062bbd3bd852ce95d7d44056a91
DavidME1604 <davidestebanmoralese@gmail.com>
2024-11-15 10:58:02 -0500

    generate chart

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ app = Flask(__name__)
 CORS(app)
 
 
+main_data = {}
+
 @app.route('/api/calculate', methods=['POST'])
 def calculate():
     data = request.json
@@ -14,11 +16,24 @@ def calculate():
     final_capital = data.get('finalCapital', 0)
     num_periods = data.get('numPeriods', 0)
     periodic_contribution = data.get('periodicContribution', 0)
+    main_data['initialCapital'] = initial_capital
+    main_data['finalCapital'] = final_capital
+    main_data['numPeriods'] = num_periods
+    main_data['periodicContribution'] = periodic_contribution
 
     result = obtener_interes(initial_capital, final_capital, num_periods, periodic_contribution)
 
     return jsonify({'result': result})
 
+
+@app.route('/api/chart', methods=['POST'])
+def chart():
+    initial_capital = main_data.get('initialCapital', 0)
+    final_capital = main_data.get('finalCapital', 0)
+    num_periods = main_data.get('numPeriods', 0)
+    periodic_contribution = main_data.get('periodicContribution', 0)
+
+    return jsonify({'result': result})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
---
+++ chart.py
@@ -0,0 +1,33 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Definir la función que genera otra función
+def f(v0, interes, a):
+    def funcion(x):
+        return v0 * (1 + interes) ** x + a * (((1 + interes) ** x - (1 + interes)) / interes)
+    return funcion
+
+def graficar():
+
+    funcion_reemplazada = f(100, 0.0015, 5)
+    x = np.linspace(1, 12, 60)
+
+    y = funcion_reemplazada(x)
+
+    plt.figure(figsize=(10, 6))
+    plt.plot(x, y, color='blue', linewidth=2, label=r'$f(x) = 100 \cdot (1 + 0.0015)^x + 5 \cdot \frac{(1 + 0.0015)^x - (1 + 0.0015)}{0.0015}$')
+
+    plt.title('Gráfica de la Función Generada', fontsize=16)
+    plt.xlabel('x', fontsize=14)
+    plt.ylabel('f(x)', fontsize=14)
+    plt.axhline(0, color='black', linewidth=0.8)
+    plt.axvline(0, color='black', linewidth=0.8)
+    plt.grid(True, linestyle='--', alpha=0.7)
+    plt.legend()
+
+    plt.savefig('Img/grafico_funcion.png', format='png', dpi=300, bbox_inches='tight')
+
+    plt.show()
+
+graficar()
+
