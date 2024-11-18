30caff4af14e60495c0444a52e5b59208b7cc261
DavidME1604 <davidestebanmoralese@gmail.com>
2024-11-18 13:10:35 -0500

    fix table

--- app.py
+++ app.py
@@ -7,8 +7,10 @@ app = Flask(__name__)
 CORS(app)
 
 data_table = []
+
 @app.route('/api/calculate', methods=['POST'])
 def calculate():
+    global data_table  # Indicar que se va a modificar la variable global
     data = request.json
     try:
         initial_capital = float(data.get('initialCapital', 0))
@@ -16,16 +18,20 @@ def calculate():
         num_periods = int(data.get('numPeriods', 0))
         periodic_contribution = float(data.get('periodicContribution', 0))
         frequency = data.get('frequency')
+
         if initial_capital < 0 or final_capital < 0 or num_periods <= 0 or periodic_contribution < 0:
             return jsonify({'error': 'Los valores ingresados deben ser positivos.'}), 400
+
         result = obtener_interes(initial_capital, final_capital, num_periods, periodic_contribution)
         graficar(initial_capital, periodic_contribution, result)
+
+        # Actualizar la variable global data_table
         data_table = table_data(initial_capital, num_periods, periodic_contribution, result)
+
         return jsonify({'result': result})
     except Exception as e:
         print(f"Error en /api/calculate: {e}")
         return jsonify({'error': 'Ocurrió un error al procesar la solicitud.'}), 500
-
 
 @app.route('/api/chart', methods=['POST'])
 def chart():
