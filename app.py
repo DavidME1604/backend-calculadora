6d74a5374620615f57068e2266345da54ac93924
DavidME1604 <davidestebanmoralese@gmail.com>
2024-11-26 22:46:59 -0500

    fix data table

--- app.py
+++ app.py
@@ -6,30 +6,29 @@ from chart import graficar, table_data
 app = Flask(__name__)
 CORS(app)
 
-data_table = []
+initial_capital = 0
+final_capital = 0
+num_periods = 0
+periodic_contribution = 0
+interest = 0
 
 @app.route('/api/calculate', methods=['POST'])
 def calculate():
-    global data_table  # Indicar que se va a modificar la variable global
+    global initial_capital, final_capital, num_periods, periodic_contribution, interest  # Indicar que se va a modificar la variable global
     data = request.json
-    print(f"Datos recibidos: {data}")
     try:
         initial_capital = float(data.get('initialCapital', 0))
         final_capital = float(data.get('finalCapital', 0))
         num_periods = int(data.get('numPeriods', 0))
         periodic_contribution = float(data.get('periodicContribution', 0))
         frequency = str(data.get('frequency', 0))
-        print(frequency)
 
         if initial_capital < 0 or final_capital < 0 or num_periods <= 0 or periodic_contribution < 0:
             return jsonify({'error': 'Los valores ingresados deben ser positivos.'}), 400
-        result = obtener_interes(initial_capital, final_capital, num_periods, periodic_contribution)
-        graficar(initial_capital, periodic_contribution, result,frequency)
+        interest = obtener_interes(initial_capital, final_capital, num_periods, periodic_contribution)
+        graficar(initial_capital, periodic_contribution, interest,frequency)
 
-        # Actualizar la tabla global con nuevos cálculos
-        data_table = table_data(initial_capital, num_periods, periodic_contribution, result)
-
-        return jsonify({'result': result})
+        return jsonify({'result': interest})
     except Exception as e:
         print(f"Error en /api/calculate: {e}")
         return jsonify({'error': 'Ocurrió un error al procesar la solicitud.'}), 500
@@ -42,30 +41,13 @@ def chart():
 
 @app.route('/api/table', methods=['POST'])
 def table():
+    global initial_capital, final_capital, num_periods, periodic_contribution, interest
     try:
         data = request.json
         required_rows = int(data.get('requiredRows', 5))
-        global data_table
+        data_table = table_data(initial_capital, required_rows, periodic_contribution, interest)
 
-        if not data_table:
-            return jsonify({'error': 'No hay datos calculados. Por favor, realiza un cálculo primero.'}), 400
-
-        if required_rows > len(data_table):
-            last_row = data_table[-1]
-            initial_capital = last_row['capital']
-            periodic_contribution = last_row['contribution']
-            interest = 0.20171  # Ejemplo: tasa de interés fija
-            last_period = last_row['period']
-            additional_data = table_data(
-                initial_capital,
-                required_rows - len(data_table),
-                periodic_contribution,
-                interest,
-                start_period=last_period + 1
-            )
-            data_table.extend(additional_data)
-
-        return jsonify({'dataTable': data_table[:required_rows]})
+        return jsonify({'dataTable': data_table})
     except Exception as e:
         print(f"Error en /api/table: {e}")
         return jsonify({'error': 'Ocurrió un error al procesar la solicitud.'}), 500
